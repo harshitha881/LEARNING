14302be32a4b2e15e672f93766c2707057b54cd0
harshitha881 <126486269+harshitha881@users.noreply.github.com>
2023-08-11 00:01:24 +0530

    PROGRAMS ON METHODS
    ALL PROGRAMS BELONGS TO CONCEPTCALLED METHODS

---
+++ calculator/src/calculator/calculatorAPP.java
@@ -0,0 +1,18 @@
+package calculator;
+
+
+
+public class calculatorAPP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calculator calculator=new Calculator();
+calculator.num1=20;
+calculator.num1=10;
+calculator.add()
+calculator.sub()
+calculator.multi()
+calculator.div()
+	}
+
+}
---
+++ double/src/trouble.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class trouble {
+public static void main (String[] args){
+	
+
+
+	
+Scanner scan=new Scanner(System.in);
+
+System.out.println("double the number");
+int num=scan.nextInt();
+System.out.println(doubleTheNumber(num));
+}
+public static int doubleTheNumber(int num1) {
+return num1*2;
+}
+}
+
